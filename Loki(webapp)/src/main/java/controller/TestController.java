688f6b630b3f9e1235894a84d60516893b1b946c
4某人 <594180536@qq.com>
2019-06-04 00:44:42 +0800

    奥丁项目启动

---
+++ Loki(webapp)/src/main/java/controller/TestController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hi")
+    @ResponseBody
+    public String test(){
+        return "可能我也会想呢";
+    }
+}
